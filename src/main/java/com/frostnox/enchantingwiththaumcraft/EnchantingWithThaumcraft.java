87c4a95b2c688509cd6fd26837d6e8aad2b35d46
Frostnox <51527640+Frostnox@users.noreply.github.com>
2019-08-22 21:48:20 -0700

    Add files via upload

--- src/main/java/com/frostnox/enchantingwiththaumcraft/EnchantingWithThaumcraft.java
+++ src/main/java/com/frostnox/enchantingwiththaumcraft/EnchantingWithThaumcraft.java
@@ -17,7 +17,7 @@ import thaumcraft.api.research.ResearchCategories;
 public class EnchantingWithThaumcraft {
 	public static final String MODID = "enchantingwiththaumcraft";
 	public static final String NAME = "Enchanting With Thaumcraft";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 	public static final String DEPENDENCIES = "required-after:thaumcraft";
 	
 	public static Logger LOGGER;
